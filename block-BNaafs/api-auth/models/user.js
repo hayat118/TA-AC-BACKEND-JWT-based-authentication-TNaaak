ad89c21cd8b17fb7e2724e9cc8ca7c1132146873
ravi11o <ravi.suraj110@gmail.com>
2022-12-23 17:58:20 +0530

    validating token

---
+++ block-BNaafs/api-auth/models/user.js
@@ -0,0 +1,51 @@
+var mongoose=require('mongoose');
+var bcrypt=require('bcrypt')
+var jwt=require('jsonwebtoken')
+var Schema=mongoose.Schema;
+
+var userSchema= new mongoose.Schema({
+  name:{type:String, required:true},
+  email:{type:String, required:true, unique:true},
+  password:{type:String, minlength:5, required:true}
+
+},{timestamps:true})
+
+userSchema.pre("save", async function(next){
+  if(this.password && this.isModified('password')){
+    this.password= await bcrypt.hash(this.password, 10);
+  }
+  next()
+})
+
+userSchema.methods.verifyPassword= async function(password){
+  try {
+    var result= await bcrypt.compare(password, this.password);
+    return result
+  } catch (error) {
+    return error;
+  }
+  bcrypt.compare();
+}
+
+userSchema.methods.signToken= async function(){
+  // console.log(this)
+  var payload= {userId:this.id, email:this.email};
+  try {
+    var token=await jwt.sign(payload, "thisissecret");
+    return token;
+  } catch (error) {
+    return error;
+  }
+}
+
+userSchema.methods.userJSON=function(token){
+  return{
+    name:this.name,
+    email:this.email,
+    token:token
+  }
+}
+
+var User=mongoose.model("User", userSchema)
+
+module.exports=User;
